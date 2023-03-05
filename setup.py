9e9ec7a9f2f4ec8e00293e8ba02c73054eb737fc
Semih <50242428+semihorhanbio@users.noreply.github.com>
2023-03-05 11:41:49 +0000

    create generic project structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''this function will return the list of requirements'''
+
+    with open(file_path) as file_obj:
+        requirements = [req.strip() for req in file_obj.readlines()]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)    
+    
+    return requirements
+
+setup(
+name='mlopsproject',
+version='0.0.1',
+author='Semih Orhan',
+author_email='sorhan3.14@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
